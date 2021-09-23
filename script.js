f563b2619561980930f6375e88c66ab03c04a9b6
Ali Madooei <alimadooei@gmail.com>
2021-09-23 01:18:17 -0400

    Step 12: `getLocationKey`

--- script.js
+++ script.js
@@ -11,8 +11,13 @@ function getWeatherForecast(event) {
 }
 
 function getLocationKey(city) {
-  // TODO get the "location key" for the given `city`!
-  //  then call getCurrentCondition to retrieve weather forecast for it!
+  fetch(`${BASE_URL}/locations/v1/cities/search?apikey=${API_KEY}&q=${city}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const location = data[0];
+      getCurrentCondition(location);
+    })
+    .catch((err) => console.log(err));
 }
 
 function getCurrentCondition(location) {
